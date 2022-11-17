a0bbbe7600dd80cbbc025c0541f12b2fad280e40
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2022-11-17 17:35:16 -0500

    my-map

--- practice-for-week-04-tdd-style-long-practice-main/problems/my-map.js
+++ practice-for-week-04-tdd-style-long-practice-main/problems/my-map.js
@@ -1,5 +1,11 @@
 function myMap(inputArray, callback) {
   // Your code here
+  let newArray = [];
+  for (let value of inputArray) {
+    let map = callback(value);
+    newArray.push(map)
+  }
+  return newArray
 }
 
-module.exports = myMap;
+module.exports = {myMap};
--- practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/my-map-spec.js
@@ -1,1 +1,17 @@
 // Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const { myMap } = require("../problems/my-map");
+
+describe("myMap function", function () {
+    it("should replicate myMap", function () {
+        const inputArray = [1, 2, 3];
+        const callback = (value) => value * 2
+        const result = myMap(inputArray, callback);
+        const expected = [2, 4, 6];
+        expect(result).to.eql(expected);
+
+        //eql used for arrays for deep equal (==)
+        //equal used for strict equal (===)
+    });
+});
