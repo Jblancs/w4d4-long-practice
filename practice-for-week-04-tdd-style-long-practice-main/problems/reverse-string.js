753f75cd757f602dfbdc7675dcb9f8689a3ec872
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2022-11-17 16:43:30 -0500

    reverstring

--- practice-for-week-04-tdd-style-long-practice-main/problems/reverse-string.js
+++ practice-for-week-04-tdd-style-long-practice-main/problems/reverse-string.js
@@ -1,3 +1,11 @@
-module.exports = function reverseString(string) {
+
+function reverseString(string) {
   // Your code here
+  let newWord = ""
+  for (let i = string.length - 1; i >= 0; i--) {
+    newWord += string[i]
+  }
+  return newWord
 };
+
+module.exports = { reverseString }
--- practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/reverse-string-spec.js
@@ -1,1 +1,13 @@
 // Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const { reverseString } = require("../problems/reverse-string")
+
+describe("reverseString function", function () {
+    it("should return string in reverse", function () {
+        const word = 'fun'
+        const result = reverseString(word)
+        const expected = 'nuf'
+        expect(result).to.equal(expected)
+    })
+})
