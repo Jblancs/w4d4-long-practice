2d5a39d61a45da6aec6cc335cb537ce9d2127ff2
Emir <emirusubaliev@Emirs-MacBook-Pro.local>
2022-11-17 18:15:38 -0600

    almost finished

--- practice-for-week-04-tdd-style-long-practice-main/problems/person.js
+++ practice-for-week-04-tdd-style-long-practice-main/problems/person.js
@@ -1,5 +1,37 @@
 class Person {
   // Your code here
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  sayHello() {
+    return `Hello ${this.name}`
+  }
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson}`
+  }
+  switchVisit(otherPerson) {
+    return this.visit(otherPerson)
+  }
+  update(obj) {
+    if (typeof obj !== "object") {
+      throw new TypeError("not object!")
+    }
+    if (!obj.name || !obj.age) {
+      throw new TypeError("give name or age")
+    }
+    let newName = this.name = obj.name
+    let newAge = this.age = obj.age
+    
+  }
+  tryUpdate(obj) {
+
+  }
+  static greatAll(...obj) {
+    let arr = [];
+    obj.forEach(el => arr.push(sayHello(el)))
+  }
 }
+
 
 module.exports = Person;
--- practice-for-week-04-tdd-style-long-practice-main/test/person-spec.js
+++ practice-for-week-04-tdd-style-long-practice-main/test/person-spec.js
@@ -1,1 +1,48 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const Person  = require("../problems/person");
+
+describe("Person", function () {
+  // beforeEach
+
+  let ironman;
+  let cap;
+
+  beforeEach(() => {
+    ironman = new Person('Robert', 43);
+    cap = new Person('Chris', 38);
+  })
+
+
+  describe("Person constructor function", function () {
+    it('should have a "name" and "age" property', function () {
+      expect(ironman).to.have.property("name")
+      expect(cap).to.have.property("name")
+      expect(ironman).to.have.property("age")
+      expect(cap).to.have.property("age")
+    });
+
+    it('should set the "name" and "age" property when a new Person is created', function () {
+      expect(ironman.name).to.equal("Robert")
+      expect(cap.name).to.equal("Chris")
+      expect(ironman.age).to.equal(43)
+      expect(cap.age).to.equal(38)
+    });
+    describe("invoker instances functions", function () {
+        it("Should say hello with sayHello function", function () {
+            expect(ironman.sayHello()).to.equal(`Hello Robert`)
+        })
+        it("Should visit with visit function", function () {
+            expect(cap.visit("Robert")).to.equal(`Chris visited Robert`)
+        })
+        it("Should visit with visit function", function () {
+            expect(ironman.switchVisit("Chris")).to.equal(`Robert visited Chris`)
+        })
+        it("Should visit with visit function", function () {
+            expect(ironman.update(cap)).to.equal(cap)
+
+        })
+    })
+
+  });
+})
